8b2245d37534143550d343c649e08e55e9bd57ba
jiankaiking <www.1292934138@qq.com>
2020-09-16 21:20:21 +0800

    login修改

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,16 +1,18 @@
 <template>
-  <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="login-form">
-    <el-form-item label="账号" prop="username">
-      <el-input  v-model="ruleForm.username"></el-input>
-    </el-form-item>
-    <el-form-item label="密码" prop="password">
-      <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-      <el-button @click="resetForm('ruleForm')">重置</el-button>
-    </el-form-item>
-  </el-form>
+  <div class="login-connner">
+    <div class="login-bg"></div>
+    <el-form :model="ruleForm" size="medium" content status-icon :rules="rules" ref="ruleForm" class="login-form">
+      <el-form-item prop="username">
+        <el-input prefix-icon="el-icon-user" v-model="ruleForm.username"></el-input>
+      </el-form-item>
+      <el-form-item  prop="password">
+        <el-input prefix-icon="el-icon-lock" show-password type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" style="width: 100%;" @click="submitForm('ruleForm')">提交</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -20,8 +22,8 @@ export default {
   data() {
     return {
       ruleForm: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
       },
       rules: {
         username: [
@@ -59,7 +61,20 @@ export default {
 </script>
 
 <style scoped>
+  .login-connner{
+    display: flex; align-items: center; justify-content: center; width: 100%; height: 100%;
+  }
+  .login-bg{
+    height: 100%; width: 100%;
+    position: absolute;
+    background-size:cover;
+    background: url("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1600271558754&di=bba0ff3385d5b9a97bd25a8269834ea7&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F2020-02-25%2F5e54ceb6e2b0d.png") no-repeat center 0;
+  }
+  .login-bg::before{
+    position: absolute; content: ""; background: rgba(0,0,0,.7); height: 100%; width: 100%;
+  }
 .login-form{
-  width: 500px;
+  border-radius: 8px;
+  width: 300px; position: absolute; right: 50%; margin-right: -200px; padding:55px 35px 45px;
 }
 </style>
